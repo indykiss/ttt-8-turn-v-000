107853d6d7b856b5fec17d09cdeebfa9794760c0
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-07-26 15:48:59 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,10 +1,8 @@
-# Helper methods
 
 def turn(board)
   puts "Please enter 1-9:"
-
-user_input = gets.strip
-converted_input = input_to_index (user_input)
+  user_input = gets.strip
+  converted_input = input_to_index (user_input)
 
 if valid_move?(board, converted_input) 
   move(board, converted_input)
